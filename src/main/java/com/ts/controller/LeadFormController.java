7f59ba8781c57a2ea1d05fefd5c60f0118179744
kirankumarpal <kiranuttampal1997@gmail.com>
2023-10-23 02:13:14 +0530

    Modified Lead

--- src/main/java/com/ts/controller/LeadFormController.java
+++ src/main/java/com/ts/controller/LeadFormController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,15 +51,36 @@ public class LeadFormController {
 			return ResponseEntity.badRequest().body("Email parameter is missing or empty.");
 		}
 	}
-	
+
 	@GetMapping("/getAllLeads")
 	public List<LeadForm> getAllLeads(LeadForm getAllLeads) {
 		return ls.getAllLeads(getAllLeads);
 	}
-	
+
 	@GetMapping("search")
-    public List<LeadForm> searchLeads(@RequestParam(value = "data", required = false) String query) {
-        return ls.searchLeads(query);
-    }
+	public List<LeadForm> searchLeads(@RequestParam(value = "data", required = false) String query) {
+		return ls.searchLeads(query);
+	}
+
+	@PutMapping("/update/{email}")
+	public ResponseEntity<String> updateLead(@RequestParam("leadName") String leadName,
+			@RequestParam("email") String email, @RequestParam("mobileNo") String mobileNo,
+			@RequestParam("address") String address, @RequestParam("degree") String degree,
+			@RequestParam("field") String field, @RequestParam("collegeName") String collegeName,
+			@RequestParam("passingYear") String passingYear, @RequestParam("courseName") String courseName,
+			@RequestParam("mode") String mode, @RequestParam("experience") String experience,
+			@RequestParam("yearsOfExperience") String yearsOfExperience,
+			@RequestParam("designation") String designation,
+			@RequestParam("courseDoneFromOtherInstitute") String courseDoneFromOtherInstitute,
+			@RequestParam("instituteName") String instituteName,
+			@RequestParam("reasonForChanging") String reasonForChanging, @RequestParam("interest") String interest,
+			@RequestParam("approach") String approach, @RequestParam("referenceName") String referenceName,
+			@RequestParam("batchCode") String batchCode, @RequestParam("status") String status,
+			@RequestParam("reasonPostpone") String reasonPostpone) {
+		String lead = ls.updateLead(leadName, email, mobileNo, address, degree, field, collegeName, passingYear,
+				courseName, mode, experience, yearsOfExperience, designation, courseDoneFromOtherInstitute,
+				instituteName, reasonForChanging, interest, approach, referenceName, batchCode, status, reasonPostpone);
+		return ResponseEntity.ok(lead);
+	}
 
 }
--- src/main/java/com/ts/service/LeadFormService.java
+++ src/main/java/com/ts/service/LeadFormService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -138,6 +140,101 @@ public class LeadFormService {
 
 	public List<LeadForm> searchLeads(String query) {
 		return lr.searchByCriteria(query);
+	}
+
+	public String updateLead(String leadName, String email, String mobileNo, String address, String degree,
+			String field, String collegeName, String passingYear, String courseName, String mode, String experience,
+			String yearsOfExperience, String designation, String courseDoneFromOtherInstitute, String instituteName,
+			String reasonForChanging, String interest, String approach, String referenceName, String batchCode,
+			String status, String reasonPostpone) {
+		LeadForm lf = lr.findByEmail(email)
+				.orElseThrow(() -> new EntityNotFoundException("Lead not found with email: " + email));
+		if (leadName == null || leadName.isEmpty() || email == null || email.isEmpty() || mobileNo == null
+				|| mobileNo.isEmpty() || address == null || address.isEmpty() || degree == null || degree.isEmpty()
+				|| field.isEmpty() || mode.isEmpty() || experience.isEmpty() || courseDoneFromOtherInstitute.isEmpty()
+				|| interest == null || approach.isEmpty() || status.isEmpty()) {
+			return "Please Enter all details.";
+		}
+
+		String[] nameWords = leadName.split(" ");
+		StringBuilder formattedName = new StringBuilder();
+
+		for (String word : nameWords) {
+			if (!word.isEmpty()) {
+				if (formattedName.length() > 0) {
+					formattedName.append(" ");
+				}
+				formattedName.append(word.substring(0, 1).toUpperCase());
+				formattedName.append(word.substring(1).toLowerCase());
+			}
+		}
+
+		lf.setLeadName(formattedName.toString());
+
+		lf.setMobileNo(mobileNo);
+		String mobilePattern = "^[6789]\\d{9}$";
+		Pattern mobileRegex = Pattern.compile(mobilePattern);
+		Matcher mobileMatcher = mobileRegex.matcher(mobileNo);
+
+		if (!mobileMatcher.matches()) {
+			return "Invalid mobile number.";
+		}
+
+		lf.setAddress(address);
+		lf.setDegree(degree);
+		lf.setField(field);
+		lf.setCollegeName(collegeName);
+		lf.setPassingYear(passingYear);
+		lf.setCourseName(courseName);
+		lf.setMode(mode);
+		lf.setExperience(experience);
+
+		if ("IT".equalsIgnoreCase(lf.getExperience())) {
+			lf.setYearsOfExperience(yearsOfExperience);
+			lf.setDesignation(designation);
+		} else {
+			lf.setYearsOfExperience(null);
+			lf.setDesignation(null);
+		}
+
+		lf.setCourseDoneFromOtherInstitute(courseDoneFromOtherInstitute);
+
+		if ("Yes".equalsIgnoreCase(lf.getCourseDoneFromOtherInstitute())) {
+			lf.setInstituteName(instituteName);
+			lf.setReasonForChanging(reasonForChanging);
+		} else {
+			lf.setInstituteName(null);
+			lf.setReasonForChanging(null);
+		}
+
+		lf.setInterest(interest);
+		lf.setApproach(approach);
+
+		if ("Reference".equalsIgnoreCase(lf.getApproach())) {
+			lf.setReferenceName(referenceName);
+			lf.setBatchCode(batchCode);
+		} else {
+			lf.setReferenceName(null);
+			lf.setBatchCode(null);
+		}
+
+		lf.setStatus(status);
+
+		if ("PostPone".equalsIgnoreCase(lf.getStatus())) {
+			lf.setReasonPostpone(reasonPostpone);
+		} else {
+			lf.setReasonPostpone(null);
+		}
+
+		if ("Cancel".equalsIgnoreCase(lf.getStatus())) {
+			lf.setReasonPostpone(reasonPostpone);
+		} else {
+			lf.setReasonPostpone(null);
+		}
+
+		lr.save(lf);
+		return "Lead has been updated successfully";
+
 	}
 
 }
